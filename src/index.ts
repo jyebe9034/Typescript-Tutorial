1879f7a2d40447d7efa3d8cd8c71bd62ed8450c8
hannah <jyebe9034@gmail.com>
2026-01-13 14:38:19 +0900

    Inicial commit with basic typescript

---
+++ src/index.ts
@@ -0,0 +1,14 @@
+// No need to annatate all the types.
+// let sales: number = 123_456_789;
+// let course: string = 'Typescript';
+// let is_published: boolean = true;
+// let level: any;
+
+let sales = 123_456_789;
+let course = 'Typescript';
+let is_published = true;
+let level;
+
+function render(document: any) {
+    console.log(document);
+}
